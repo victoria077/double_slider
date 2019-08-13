72c45978635b1486109cf8d98d26107c774b0c1f
viktoria <vik.slepchenko@gmail.com>
2019-08-13 19:10:12 +0200

    change function for toggle

--- src/myFunc.js
+++ src/myFunc.js
@@ -1,5 +1,5 @@
 
-let toggle, bar, toggleLeft, barLeft;
+let toggle, toggleLeft, barLeft;
 function init() {
     toggle = $("#slider__toggle");
     toggle.on('mousedown', () => {
@@ -10,28 +10,76 @@ function init() {
         $(document).on('mouseup', onThumbMouseup);
     });
 }
+let LimitMovementX, thumbCoord;
 
-function moveThumb(event) {   
-    
+function moveThumb(event) {
     let bar = $("#slider__bar");
-    let barLeft;
+    let barLeft = bar.offset().left;  
+    let barWidth = bar.outerWidth();
+    let toggleWidth = toggle.outerWidth();
     LimitMovementX = {
-        // min: bar.offsetLeft,
-        // max: bar.offsetLeft + bar.offsetWidth - thumb.offsetWidth,
+        min: barLeft,
+        max: barLeft + barWidth - toggleWidth
       }
       toggleLeft = toggle.offset().left;
-      thumbCoord = toggleLeft + event.pageX;
-      // if (thumbCoord < LimitMovementX.min) {
-      //   thumbCoord = LimitMovementX.min;
-      // }
-      // if (thumbCoord > LimitMovementX.max) {
-      //   thumbCoord = LimitMovementX.max;
-      // }
-    toggle.css("left", thumbCoord);
+      console.log(event.pageX);
+      thumbCoord = toggleLeft;
+      if (thumbCoord < LimitMovementX.min) {
+        thumbCoord = LimitMovementX.min;
+      }
+      if (thumbCoord > LimitMovementX.max) {
+        thumbCoord = LimitMovementX.max;
+      }
+    let g = toggle.css("left", thumbCoord);
+   
 };
 
 function onThumbMouseup() {
-    document.removeEventListener('mousemove', moveThumb);
-    document.removeEventListener('mouseup', onThumbMouseup);
+    $(document).off('mousemove');
+    $(document).off('mouseup');
 };
 
+let f = function evPos(){
+   bar.mousemove(function( event ) {
+        return event.pageX;
+      });
+}
+init();
+
+
+// let toggle, toggleLeft, barLeft;
+// function init() {
+//     thumb = document.querySelector('.slider__toggle');
+//     thumb.addEventListener('mousedown', function() {
+//         this.addEventListener('dragstart', function(e) {
+//           e.preventDefault();
+//         });
+//         document.addEventListener('mousemove', moveThumb);
+//         document.addEventListener('mouseup', onThumbMouseup);
+//       });
+// }
+
+// let bar = document.querySelector('.slider__bar');
+// let LimitMovementX, thumbCoord, thumbCoord2;
+// function moveThumb(e) {
+//     LimitMovementX = {
+//       min: bar.offsetLeft,
+//       max: bar.offsetLeft + bar.offsetWidth - thumb.offsetWidth,
+//     }
+//     thumbCoord = thumb.offsetLeft + e.movementX;
+//     if (thumbCoord < LimitMovementX.min) {
+//       thumbCoord = LimitMovementX.min;
+//     }
+//     if (thumbCoord > LimitMovementX.max) {
+//       thumbCoord = LimitMovementX.max;
+//     }
+//     thumb.style.left = thumbCoord + 'px';
+
+//   };
+
+//   function onThumbMouseup() {
+//     document.removeEventListener('mousemove', moveThumb);
+//     document.removeEventListener('mouseup', onThumbMouseup);
+//   };
+
+//   init();
